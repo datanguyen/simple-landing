ce222575dcc4b8564cf2fd47e44eb14b39a06f99
Dat Tan Nguyen <ntdatjoker@gmail.com>
2019-08-30 12:55:53 +0700

    Add deployment script

--- gulpfile.js
+++ gulpfile.js
@@ -1,25 +1,47 @@
-'use strict';
+'use strict'
 
-var gulp = require('gulp'),
-    sass = require('gulp-sass'),
-    sourcemaps = require('gulp-sourcemaps'),
-    browserSync = require('browser-sync').create();
+const gulp = require('gulp')
+const sass = require('gulp-sass')
+const sourceMap = require('gulp-sourcemaps')
+const browserSync = require('browser-sync').create()
 
+const cleanCSS = require('gulp-clean-css')
+const htmlMin = require('gulp-htmlmin')
+const imageMin = require('gulp-imagemin')
+const fontMin = require('gulp-fontmin')
 
-gulp.task('serve', ['sass'], function() {
-   browserSync.init({
-       server: './public'
-   });
+gulp.task('sass', () => {
+  return gulp.src('./public/sass/**/*.scss')
+    .pipe(sourceMap.init())
+    .pipe(sass().on('error', sass.logError))
+    .pipe(sourceMap.write('./'))
+    .pipe(gulp.dest('./public/css'))
+    .pipe(browserSync.stream())
+})
 
-   gulp.watch('./public/sass/**/*.scss', ['sass']);
-   gulp.watch('./public/**/*.html').on('change', browserSync.reload);
-});
+gulp.task('serve', ['sass'], () => {
+  browserSync.init({
+    server: './public'
+  })
 
-gulp.task('sass', function () {
-   return gulp.src('./public/sass/**/*.scss')
-       .pipe(sourcemaps.init())
-       .pipe(sass().on('error', sass.logError))
-       .pipe(sourcemaps.write('./'))
-       .pipe(gulp.dest('./public/css'))
-       .pipe(browserSync.stream());
-});
+  gulp.watch('./public/sass/**/*.scss', ['sass'])
+  gulp.watch('./public/**/*.html').on('change', browserSync.reload)
+})
+
+gulp.task('build', ['sass'], () => {
+  gulp.src('public/css/*.css')
+    .pipe(cleanCSS({ compatibility: 'ie8' }))
+    .pipe(gulp.dest('dist/css'))
+
+  gulp.src('public/index.html')
+    .pipe(htmlMin({ collapseWhitespace: true }))
+    .pipe(gulp.dest('dist'))
+
+  gulp.src('public/images/*.png')
+    .pipe(imageMin())
+    .pipe(gulp.dest('dist/images'))
+
+  gulp.src('public/fonts/**')
+    .pipe(fontMin())
+    .pipe(gulp.dest('dist/fonts'))
+})
